8b0a5a92c3fab371c31a4cf3a627a312c578283f
Maksim Chalyh <nazgulius@yandex.ru>
2024-10-14 22:53:01 +0300

    sort OK

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,9 @@
 // TODO: write your code here
 import sum from './basic';
+import sort from './sort';
 
 console.log('worked');
 
-console.log(sum([1, 2]));
+const obj = {name: 'мечник', health: 10, level: 2, attack: 80, defence: 40}
+
+console.log(sort(obj, ["name", "level"]));
--- src/js/sort.js
+++ src/js/sort.js
@@ -1,3 +1,21 @@
-export default function sort (array) {
+export default function sort (array, rules) {
+  const arraySort = [];
+  const arraySort2 = [];
+  let i = 0;
 
+  for (const key in array) {
+    if (rules[i] === key) {
+      arraySort.push({key: key, value: array[key]});
+      i++;
+    } else {
+      arraySort2.push({key: key, value: array[key]});      
+    }
+  }
+
+  arraySort2.sort((a, b) => {    
+    if (a.key < b.key) return -1;
+    if (a.key > b.key) return 1;
+  });  
+
+  return arraySort.concat(arraySort2);
 }
