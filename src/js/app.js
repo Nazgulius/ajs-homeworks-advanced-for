caa670dedd182ddb4c0a58e18171a4e8caf84272
Maksim Chalyh <nazgulius@yandex.ru>
2024-10-17 22:48:42 +0300

    task OK

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,13 @@
 // TODO: write your code here
-import sum from './basic';
 import sort from './sort';
 
 console.log('worked');
 
-const obj = {name: 'мечник', health: 10, level: 2, attack: 80, defence: 40}
+const obj = {name: 'мечник', health: 10, level: 2, attack: 80, defence: 40};
 
 console.log(sort(obj, ["name", "level"]));
+
+
+const obj2 = { health: 10, attack: 80, defence: 40, level: 2, name: 'мечник'};
+
+console.log(sort(obj2, ["name", "level"]));
