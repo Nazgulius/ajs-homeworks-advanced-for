fca9a1556582256deed49d6397aede6676fa2232
Maksim Chalyh <nazgulius@yandex.ru>
2024-10-17 22:54:33 +0300

    tesk OK

--- src/js/__tests__/sort.test.js
+++ src/js/__tests__/sort.test.js
@@ -3,7 +3,7 @@ import sort from '../sort';
 test('sort 1', () => {
   const obj = { name: 'мечник', health: 10, level: 2, attack: 80, defence: 40};
   const received = sort(obj, ["name", "level"]);
-
+  
   const expected = [{"key": "name", "value": "мечник"}, {"key": "level", "value": 2}, 
   {"key": "attack", "value": 80}, {"key": "defence", "value": 40}, 
   {"key": "health", "value": 10}];
@@ -11,3 +11,13 @@ test('sort 1', () => {
   expect(received).toEqual(expected);
 })
 
+test('sort 2', () => {
+  const obj = { defence: 40, health: 10, attack: 80, level: 2, name: 'мечник'};
+  const received = sort(obj, ["name", "level"]);
+
+  const expected = [{"key": "name", "value": "мечник"}, {"key": "level", "value": 2}, 
+  {"key": "attack", "value": 80}, {"key": "defence", "value": 40}, 
+  {"key": "health", "value": 10}];
+
+  expect(received).toEqual(expected);
+})
--- src/js/sort.js
+++ src/js/sort.js
@@ -1,52 +1,25 @@
-export default function sort (obj, array) {
-  //const arraySort = [];
-  //const arraySort2 = [];
+export default function sort(obj, array) {
   const result = [];
-  const copyObj = {...obj };
-/*
-  for (let i = 0; i < rules.length; i++) {
-    for (const key in array) {
-      if (rules[i] === key) {
-        arraySort.push({key: key, value: array[key]});
-        i++;
-      } else{      
-        arraySort2.push({key: key, value: array[key]});
-      }           
-    }    
-  }*/
-  
+  const copyObj = { ...obj };
+
   array.forEach((elem) => {
     result.push({ key: elem, value: copyObj[elem] });
     delete copyObj[elem];
-    });
-    
-    const temp = [];
-    for (const key in copyObj) {
+  });
+
+  const temp = [];
+  for (const key in copyObj) {
     temp.push({ key, value: copyObj[key] });
-    }
+  }
 
-    temp.sort((a, b) => {
-      if (a.key < b.key) return -1;
-      if (a.key > b.key) return 1;
-      });
-      
-      const arrKeys = Object.keys(copyObj).sort();
-      arrKeys.forEach((elem) => {
-      result.push({ key: elem, value: copyObj[elem] });
-      });
-/*
-  arraySort2.sort((a, b) => {    
-    if (a.key < b.key) {
-      return -1
-    } else if (a.key > b.key) {
-      return 1;
-    }
-  });  
+  temp.sort((a, b) => {
+    if (a.key < b.key) return -1;
+  });
 
-  for (const key in arraySort) {
-    console.log('1 array: ' + arraySort[key].key + ' - ' + arraySort[key].value);
-  }*/
+  const arrKeys = Object.keys(copyObj).sort();
+  arrKeys.forEach((elem) => {
+    result.push({ key: elem, value: copyObj[elem] });
+  });
 
-  //return arraySort.concat(arraySort2);
   return result;
 }
